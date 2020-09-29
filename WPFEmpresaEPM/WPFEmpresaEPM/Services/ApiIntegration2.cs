8d6abbab93eff7c9b558903d64958f3c9b471a12
dcordobag <32066181+dcordobag@users.noreply.github.com>
2020-09-29 16:36:48 -0700

    ok

---
+++ WPFEmpresaEPM/WPFEmpresaEPM/Services/ApiIntegration2.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WPFEmpresaEPM.Classes;
+
+namespace WPFEmpresaEPM.Services
+{
+    public  class ApiIntegration2
+    {
+        #region "Referencias"
+        private HttpClient client;
+        #endregion
+
+        #region "Constructor"
+        public ApiIntegration2()
+        {
+            try
+            {
+                client = new HttpClient
+                {
+                    BaseAddress = new Uri(Utilities.GetConfiguration("basseAddresEPM"))
+                };
+            }
+            catch (Exception ex)
+            {
+                Error.SaveLogError(MethodBase.GetCurrentMethod().Name, this.GetType().Name, ex, ex.ToString());
+            }
+        }
+        #endregion
+
+        #region "Métodos"
+        #endregion
+    }
+}
